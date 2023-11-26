a221876c8ca0f6ef0c069e1a30824edceb750835
PabloAFT <pafiltorres@gmail.com>
2023-11-26 16:57:46 -0300

    Adição de Login e segurança
    Configurações de segurança
    Api de login

---
+++ src/main/java/trabalho/de/web/web/auth/LoginController.java
@@ -0,0 +1,66 @@
+package trabalho.de.web.web.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
+import trabalho.de.web.web.user.UserService;
+
+@RestController
+public class LoginController {
+     
+
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+
+    
+    public LoginController(AuthenticationManager authenticationManager, UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+    }
+
+  
+    @PostMapping("/api/login")
+	public ResponseEntity<Void> login( @RequestBody LoginRequest loginRequest, HttpServletResponse httpServletResponse) {
+        System.err.println(loginRequest.getPassword() + loginRequest.getUsername());
+		Authentication authenticationRequest =
+			UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.getUsername(), loginRequest.getPassword());
+		Authentication authenticationResponse =
+			this.authenticationManager.authenticate(authenticationRequest);
+            System.out.println(authenticationResponse.isAuthenticated());
+        SecurityContextHolder.getContext().setAuthentication(authenticationResponse);
+		if (authenticationResponse.isAuthenticated()) {
+
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(403).build();
+        }
+        
+	}
+    @GetMapping("/api/login")
+    public void method(HttpServletResponse httpServletResponse) {
+    httpServletResponse.setHeader("Location", "/api/users");
+    httpServletResponse.setStatus(302);
+}
+
+    @GetMapping("/logout")
+public ResponseEntity<Void> logout() {
+    try {
+        SecurityContextHolder.clearContext();
+        return ResponseEntity.ok().build();
+    } catch (Exception e) {
+        return ResponseEntity.badRequest().build();
+    }
+    
+}
+
+    
+}
---
+++ src/main/java/trabalho/de/web/web/configs/SecurityConfiguration.java
@@ -0,0 +1,76 @@
+package trabalho.de.web.web.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+
+
+import trabalho.de.web.web.user.UserService;
+
+@EnableWebSecurity()
+@EnableMethodSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                // .cors(withDefaults())
+                .csrf((csrf) -> csrf.disable())
+
+                .authorizeHttpRequests((authorize) -> authorize
+
+                        .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/auth/me").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/login.html").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/index.html").permitAll()
+
+                        .anyRequest().authenticated()
+
+                )
+                .formLogin(login -> login.loginPage("/login.html"))
+
+                .securityContext((securityContext) -> securityContext
+                        .requireExplicitSave(false)
+                        .securityContextRepository(new DelegatingSecurityContextRepository(
+                                new RequestAttributeSecurityContextRepository(),
+                                new HttpSessionSecurityContextRepository())))
+
+        // .httpBasic(Customizer.withDefaults()).logout(Customizer.withDefaults())
+        // .formLogin(Customizer.withDefaults())
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserService userDetailService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
