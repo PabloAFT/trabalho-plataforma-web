c7b357f30838cc66a2b191cda29bf6924f5b6994
PabloAFT <pafiltorres@gmail.com>
2023-11-26 16:57:01 -0300

    Configurações MongoDB (repositório e propriedades)

--- src/main/java/trabalho/de/web/web/user/UserRepository.java
+++ src/main/java/trabalho/de/web/web/user/UserRepository.java
@@ -2,8 +2,10 @@ package trabalho.de.web.web.user;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends JpaRepository<User, Long > {
+
+
+public interface UserRepository extends MongoRepository<User, String > {
     Optional<User> findByUsername(String username);
 }
